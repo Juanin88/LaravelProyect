e70c8eab83914d696e686c11d7ee2cd87b7ca58d
juan <ing.juan.garfias@gmail.com>
2016-09-07 23:40:02 -0500

    Layouts y primera plantilla.

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace LaravelProyect\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+//use LaravelProyect\Http\Requests;
+use LaravelProyect\Categoria;
+use Illuminate\Support\Facades\Redirect;
+use LaravelProyect\Http\Requests\CategoriaFormRequest;
+//use Illuminate\Support\Facades\DB;
+use DB;
+
+class CategoriaController extends Controller
+{
+    public function __contruct(){
+	}
+    
+	public function index(Request $request){
+		if ($request) {
+			$query = trim ($request->get('searchText'));
+			$categorias = DB::table('categoria')
+			->where('nombre','LIKE','%'.$query.'%')
+			//->where('','','')
+			->orderBy('idcategoria','desc')
+			->paginate(7);
+			return view('almacen.categoria.index',
+					['categorias'=>$categorias,
+					 'searchText'=>$query
+			]);
+		}
+	}
+	
+	public function create(){
+		return view('almacen.categoria.create');
+	}
+	
+	public function store(CategoriaFormRequest $request){
+		$categoria = new Categoria();
+		$categoria->nombre = $request->get('nombre');
+		$categoria->descipcion = $request->get('descripcion');
+		$categoria->condicion='1';
+		$categoria->save();
+		return Redirect::to('almacen/categoria');
+		
+		
+	}
+	
+	public function show(){
+		return view('almacen.categoria.show',
+				['categoria'=>Categoria::findOrFail($id)]);
+		
+	}
+	
+	public function edit(){
+		return view('almacen.categoria.edit',
+				['categoria'=>Categoria::findOrFail($id)]);
+	}
+	
+	public function update(CategoriaFormRequest $request, $id){
+	
+		$categoria =Categoria::findOrFail($id);
+		$categoria->nombre=$reques->get('nombre');
+		$categoria->descripcion=$reques->get('descripcion');
+		$categoria->update();
+		return Redirect::to('almacen/categoria');
+		
+	}
+	
+	public function destroy($id){
+		$categoria=Categoria::findOrFail($id);
+		$categoria->condicion='0';
+		$categoria->update();
+		return Redirect::to('almacen/categoria');
+		
+		
+	}
+}
---
+++ resources/views/almacen/categoria/index.blade.php
@@ -0,0 +1,50 @@
+@extends ('layouts.admin')
+@section('contenido')
+	
+	
+	<div class="row">
+		<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+			<h3>Listado de Categor&iacute;as 
+				<a href="categoria/create"><button>Nuevo</button></a>
+			</h3>
+			@include('almacen.categoria.search')
+		</div>
+	</div>
+	
+<div class="row">
+	<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+		<div class="table-responsive">
+		<table class='table table-striped'>
+			<thead>
+				<tr>
+					<th>Id</th>
+					<th>Nombre</th>
+					<th>Descripci&oacute;n</th>
+					<th>Opciones</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach ($categorias as $cat)
+				<tr>
+					<td>{{ $cat->idcategoria }}</td>
+					<td>{{ $cat->nombre }}</td>
+					<td>{{ $cat->descripcion }}</td>
+					<td>
+						<a href="">
+							<button class="btn btn-info">Editar</button>	
+						</a>
+						<a href="">
+							<button class="btn btn-info">Eliminar</button>	
+						</a>
+					</td>
+				</tr>
+				@endforeach
+			</tbody>
+		</table>
+		
+		</div>
+	</div>
+</div>
+	
+	
+@endsection
